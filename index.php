0fe6ce89467bd4f6287bc0d1e8e3b98f4124c81a
jared <jared@gurrfamily.com>
2021-03-12 21:16:32 -0700

    removed php code from index.html so that I can see annotations, autocomplete and syntax highlighting in php file.

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+$dbhost = 'localhost:3306';
+$dbuser = 'root';
+$dbpass = 'super03';
+$dbdatabase = 'phptest';
+$mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbdatabase, 8886);
+
+
+if(! $conn ){
+    echo 'Connected failure\n';
+}
+echo 'Connected successfully\n';
+
+$sql = "";
+header("Location: index.html");
