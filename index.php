9231acb8bd3a3269b0fe0cc050ed95625a3b1f5b
jgurr4 <jared@gurrfamily.com>
2021-03-17 12:46:15 -0700

    Created docker-compose lamp setup

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>login</title>
+</head>
+<body>
 <?php
 $dbhost = 'localhost:3306';
 $dbuser = 'root';
@@ -6,10 +13,22 @@ $dbdatabase = 'phptest';
 $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbdatabase, 8886);
 
 
-if(! $conn ){
-    echo 'Connected failure\n';
-}
-echo 'Connected successfully\n';
+#if(! $conn ){
+#    echo 'Connected failure\n';
+#}
+#echo 'Connected successfully\n';
 
 $sql = "";
-header("Location: index.html");
+?>
+<form action="index.php" method="post">
+    <input type="text" name="uid" placeholder="Username">
+    <br>
+    <input type="text" name="password" placeholder="password">
+    <br>
+    <input type="text" name="email" placeholder="E-mail">
+    <br>
+    <button type="submit" name="submit">signup</button>
+
+</form>
+</body>
+</html>
